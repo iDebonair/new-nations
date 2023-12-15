f9ad771ee2d1c8b81ddf35c3c93d83f856ac2a05
iDebonair <ofemiowolabi@gmail.com>
2023-12-15 10:50:01 -0700

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import OfferingForm from './components/OfferingForm';
+import LiveStream from './components/LiveStream';
+import Calendar from './components/Calendar';
+import Home from './components/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/live-stream" element={<LiveStream />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/offering" element={<OfferingForm />} /> 
+          <Route path="/" element={<Home />} /> 
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
