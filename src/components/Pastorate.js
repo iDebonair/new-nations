26ebe7c232f8b6c63fe44904812452231e99c12c
iDebonair <ofemiowolabi@gmail.com>
2024-01-23 13:23:46 -0700

    Modify who we are page

--- src/components/Pastorate.js
+++ src/components/Pastorate.js
@@ -1,51 +1,50 @@
 // Pastorate.js
 
 import React from 'react';
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, Card, CardContent, CardMedia, Grid } from '@mui/material';
 import '../styling/Pastorate.css'; // Import the CSS file
-import LeadPastor from '../assets/leadPastor.jpg'
+import LeadPastor from '../assets/leadPastor.jpg';
 
 const pastorsData = [
   {
     name: 'Rev. Dr. S.O Abimbola',
     role: 'Lead Pastor',
-    image: {LeadPastor}, // Replace with the actual image URL
+    image: LeadPastor,
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   },
   {
     name: 'Rev. D.A Adeyemo',
     role: 'Associate Pastor',
-    image: 'https://placekitten.com/301/301', // Replace with the actual image URL
+    image: LeadPastor, // Replace with the actual image URL
     bio: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   },
 ];
 
 const Pastorate = () => {
   return (
-    <Container sx={{ marginTop: '4rem', padding: '4rem' }}>
-      <div className="pastor-profiles">
-        {pastorsData.map((pastor, index) => (
-          <div className={`pastor ${index === 0 ? 'lead-pastor' : 'associate-pastor'}`} key={index}>
-            <img
-              src={pastor.image}
-              alt={pastor.name}
-              width="400"
-              height="420"
-            />
-            <div className="text-container">
-              <Typography variant="h5" gutterBottom>
-               {pastor.name}
-              </Typography>
-              <Typography variant="subtitle1" gutterBottom>
-               {pastor.role}
-              </Typography>
-              <Typography variant="body1">
-                {pastor.bio}
-              </Typography>
-            </div>
-          </div>
-        ))}
-      </div>
+    <Container sx={{ marginTop: '4rem', padding: '2rem' }}>
+      {pastorsData.map((pastor, index) => (
+        <Card key={index} sx={{ display: 'flex', marginBottom: '2rem' }}>
+          <CardMedia
+            component="img"
+            height="300"
+            image={pastor.image}
+            alt={pastor.name}
+            sx={{ width: 300, objectFit: 'cover' }}
+          />
+          <CardContent>
+            <Typography variant="h5" gutterBottom>
+              {pastor.name}
+            </Typography>
+            <Typography variant="subtitle1" gutterBottom>
+              {pastor.role}
+            </Typography>
+            <Typography variant="body1">
+              {pastor.bio}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
     </Container>
   );
 };
--- src/components/Visit.js
+++ src/components/Visit.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Grid } from '@material-ui/core';
+import { Typography, Grid, useMediaQuery, createTheme, ThemeProvider } from '@material-ui/core';
 import FAQs from './FAQs';
 import ResponsiveFAQs from './ResponsiveFAQs';
 import churchBanner from '../assets/auditorium.jpg';
@@ -8,6 +8,9 @@ import Ande from '../assets/Ande.jpg';
 const Visit = () => {
   const defaultMapLocation =
     'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15851.920346893372!2d3.3663612!3d6.5752878!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b92707ee500e9%3A0x9ee6148de43a4d22!2s1%20Aderibigbe%20Shitta%20St%2C%20Maryland%2C%20Lagos%20101233%2C%20Lagos%2C%20Nigeria!5e0!3m2!1sen!2sus!4v1614658044073!5m2!1sen!2sus';
+
+    const theme = createTheme(); // Create a simple theme
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <div style={{ padding: '0', margin: '0', position: 'relative', marginTop: '70px' }}>
@@ -20,7 +23,7 @@ const Visit = () => {
         />
         {/* Text Overlay */}
         <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center', color: 'white' }}>
-          <Typography variant="h3" style={{ fontWeight: 'bold' }} gutterBottom>
+            <Typography variant="h2" style={{ fontWeight: 'bold', fontSize: isSmallScreen ? '2rem' : '3rem', margin: 10 }} gutterBottom>
             New Nations Baptist Church
           </Typography>
           <Typography variant="body3" style={{ fontWeight: 'bold' }}>
